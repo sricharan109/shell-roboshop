13df0b91f2c072e201c35f5edc4017173191bc51
Charan Kotineni <charan.kotineni@peopletech.com>
2026-02-12 11:07:21 +0530

    added new file

--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-ID= $(id -u)
+
+ID=$(id -u)
 FOLDER="/var/log/shell-roboshop"
-FILE=/$FOLDER/$0.log
-DIR=$pwd
-if [ $ID -ne 0]; then
+FILE="$FOLDER/$0.log"
+DIR=$(pwd)
+
+if [ $ID -ne 0 ]; then
     echo "run this command with root user"
-    exit 1 
-else 
+    exit 1
+else
     echo "you are running this script with root user"
 fi
+
 mkdir -p $FOLDER
+
 VALIDATE(){
-    if [ $1 -nq 0 ]; then 
+    if [ $1 -ne 0 ]; then
         echo "$2 .....FAILED"
     else
         echo "$2 ....SUCCESS"
@@ -24,50 +28,46 @@ VALIDATE $? "Installing maven"
 id roboshop &>>$FILE
 if [ $? -ne 0 ]; then
     useradd roboshop &>>$FILE
-    VALIDATE $? "adding robothsop user"
-else 
+    VALIDATE $? "Adding roboshop user"
+else
     echo "roboshop user already exists"
 fi
 
-mkdir /app &>>$FILE
-VALIDATE $? "creating application directory"
+mkdir -p /app &>>$FILE
+VALIDATE $? "Creating application directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
-cd /app 
-unzip /tmp/shipping.zip &>>$FILE
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$FILE
+
+rm -rf /app/*
 
 cd /app
 VALIDATE $? "Moving to app directory"
 
-rm -rf /app/*
-
 unzip /tmp/shipping.zip &>>$FILE
-VALIDATE $? "Uzip shipping code"
+VALIDATE $? "Unzipping shipping code"
 
-cd /app 
 mvn clean package &>>$FILE
-VALIDATE $? "Installing and Building shipping"
+VALIDATE $? "Building shipping"
 
-mv target/shipping-1.0.jar shipping.jar 
-VALIDATE $? "Renaming the jar file"
+mv target/shipping-1.0.jar shipping.jar &>>$FILE
+VALIDATE $? "Renaming jar file"
 
 cp $DIR/shipping.service /etc/systemd/system/shipping.service &>>$FILE
-VALIDATE $? "copying systemd file"
+VALIDATE $? "Copying systemd file"
 
 dnf install mysql -y &>>$FILE
 VALIDATE $? "Installing mysql"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
-
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$FILE
     VALIDATE $? "Loaded data into MySQL"
 else
-    echo -e "data is already loaded ... SKIPPING "
+    echo "Data already loaded... SKIPPING"
 fi
 
 systemctl enable shipping &>>$FILE
-systemctl start shipping
+systemctl start shipping &>>$FILE
 VALIDATE $? "Enabled and started shipping"
